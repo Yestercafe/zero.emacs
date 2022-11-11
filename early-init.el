358d66b423c0ba8763bd213f44c51606d7aa662a
Ivan Chien <qyc027@gmail.com>
2022-11-11 19:12:18 +0800

    Add early-init.el

---
+++ early-init.el
@@ -0,0 +1,6 @@
+;;; early-init.el --- -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;;; early-init.el ends here
+
--- init.el
+++ init.el
@@ -1,9 +1,24 @@
-;;; init.el --- Load the full configuration -*- lexical-binding: t -*-
+;;; init.el --- -*- lexical-binding: t -*-
 ;;; Commentary:
-
 ;;; Code:
 
 (print (+ 1 1))
+
+(defun sanityinc/headerise-elisp ()
+  "Add minimal header and footer to an elisp buffer in order to placate flycheck."
+  (interactive)
+  (let ((fname (if (buffer-file-name)
+                   (file-name-nondirectory (buffer-file-name))
+                 (error "This buffer is not visiting a file"))))
+    (save-excursion
+      (goto-char (point-min))
+      (insert ";;; " fname " --- Insert description here -*- lexical-binding: t -*-\n"
+              ";;; Commentary:\n"
+              ";;; Code:\n\n")
+      (goto-char (point-max))
+      (insert ";;; " fname " ends here\n"))))
+(setq mac-command-modifier 'super)
+(setq mac-option-modifier 'meta)
 
 (provide 'init)
 
