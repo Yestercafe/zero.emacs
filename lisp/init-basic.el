5941c79c2bb3208863ba9156b57fc45c7bde01d7
Ivan Chien <qyc027@gmail.com>
2022-11-11 20:54:48 +0800

    Fix for CI

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -2,8 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+
+(if (display-graphic-p)
+    (progn
+      (tool-bar-mode -1)
+      (scroll-bar-mode -1)))
+
 (setq make-backup-files nil)
 (fset 'yes-or-no-p 'y-or-n-p)
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
