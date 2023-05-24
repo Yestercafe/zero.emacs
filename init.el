e37b4e308a5a448ea4efcd2c9afb714882ccc95a
Ivan Chien <qyc027@gmail.com>
2023-05-24 16:17:44 +0800

    Update gc threshold, add a snippets path

--- init.el
+++ init.el
@@ -1,8 +1,18 @@
 (setq user-init-file (or load-file-name (buffer-file-name)))
 (setq user-emacs-directory (file-name-directory user-init-file))
 
+(setq normal-gc-cons-threshold (* 20 1024 1024))
+(setq init-gc-cons-threshold (* 128 1024 1024))
+(setq gc-cons-threshold init-gc-cons-threshold)
+(add-hook 'emacs-startup-hook
+          (lambda () (setq gc-cons-threshold normal-gc-cons-threshold)))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
 (require 'org)
 (org-babel-load-file
  (expand-file-name "config.org"
 		   user-emacs-directory))
+
+(add-to-list 'load-path "~/.emacs.d/snippets")
 
