cacc7b8126e58e58747d6de25017aa0e0b6565dc
Ivan Chien <qyc027@gmail.com>
2023-10-21 13:23:50 +0800

    Add C++ quickrun

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -3,4 +3,16 @@
 (require 'eglot)
 (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
 
+(use-package quickrun
+  :ensure t
+  :commands (quickrun)
+  :bind ("C-c r" . 'quickrun)
+  :init
+  (quickrun-add-command "c++"
+						'((:command . "clang++")
+						  (:exec . ("%c -std=c++20 %o -o %e %s"
+									"%e %a"))
+						  (:remove . ("%e")))
+						:default "c++"))
+
 (provide 'init-eglot)
